c595bb75a1e0262d97c142ce16cf8f4976d09d09
Vlatko Marchan <alu0101321141@ull.edu.es>
2022-03-21 22:44:34 +0000

    Creamos las pruebas para la clase Avatar. PRUEBA y FALLA

--- tests/ejercicio1.spec.ts
+++ tests/ejercicio1.spec.ts
@@ -1,6 +1,7 @@
 import "mocha";
 import {expect} from 'chai';
 import {Pokemon} from '../src/ejercicio1/Pokemon';
+import {Avatar} from '../src/ejercicio1/Avatar';
 
 describe("Pruebas del juego de peleas.", () => {
   describe("Pruebas de la clase Pokemon", () => {
@@ -28,6 +29,41 @@ describe("Pruebas del juego de peleas.", () => {
       bellossom.setDefense(10);
       expect(bellossom.getDefense()).to.eq(10);
       bellossom.setDefense(80);
+    });
+  });
+  describe("Pruebas de la clase Avatar", () => {
+    const aang = new Avatar("Avatar", "Aang", "La venganza es como un arma de doble filo: mientras " +
+      " ves caer a tu enemigo, te envenenas por dentro", 250, 150, 100, "Avatar", "Planeador", [12, "Nomadas del aire"]);
+    const zuko = new Avatar("Avatar", "Zuko", "Yo solía pensar que esta cicatriz me marcaba. La marca " +
+      "del príncipe desterrado, condenado a perseguir al Avatar para siempre.", 150, 100, 80, "Fire", "Espadas duales Dao", [18,
+      "Nación del fuego"]);
+    const katara = new Avatar("Avatar", "Katara", "No! Yo nunca ¡Jamás! le daré la espalda a alguien que me necesite.",
+        100, 80, 40, "water", "hielo", [14, "Tibu Agua del norte"]);
+    const Toph = new Avatar("Avatar", "Toph Beifong", "No me importa cómo me veo. No busco la aprobación de nadie." +
+      " Sé muy bien quien soy.", 150, 95, 100, "land", "Cables Metal", [12, "Reino Tierra"]);
+
+
+    it("Pruebas de la definición de la clase Avatar", () => {
+      expect(aang).not.to.be.null;
+    });
+    it("Getters del universo Avatar ", () => {
+      expect(zuko.getNameUniverse()).to.eq("Avatar");
+      expect(katara.getNameCharacter()).to.eq("Katara");
+      expect(Toph.getCatchingPhrase()).to.eq("No me importa cómo me veo. No busco la aprobación de nadie." +
+      " Sé muy bien quien soy.");
+      expect(Toph.getHP()).to.eq(150);
+      expect(Toph.getDM()).to.eq(95);
+      expect(Toph.getDefense()).to.eq(100);
+      expect(Toph.getWeapon()).to.eq("Cables Metal");
+      expect(Toph.getInfo()).to.eql([12, "Reino Tierra"]);
+    });
+    it("Setter del universo Avatar ", () => {
+      Toph.setHP(10);
+      expect(Toph.getHP()).to.eq(10);
+      Toph.setHP(75);
+      Toph.setDefense(10);
+      expect(Toph.getDefense()).to.eq(10);
+      Toph.setDefense(80);
     });
   });
 });
