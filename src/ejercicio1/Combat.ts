f3bc3121b0b24441d7c656e4dc04f7bd76bca900
Vlatko Marchan <alu0101321141@ull.edu.es>
2022-03-24 08:19:36 +0000

    cout

--- src/ejercicio1/Combat.ts
+++ src/ejercicio1/Combat.ts
@@ -53,20 +53,22 @@ export class Combat {
       if (turno % 2 != 0) {
         console.log(`Turno: ${turno}`);
         console.log(`El luchador ${this.Fighter1.getNameCharacter()} realiza su ataque`);
+        console.log(`${this.Fighter1.getCatchingPhrase()}`);
         dmg = this.Fighter1.generalDamage(this.Fighter1, this.Fighter2);
         this.Fighter2.setHP(this.Fighter2.getHP() - dmg);
         console.log(`El luchador ${this.Fighter2.getNameCharacter()} recibe ${dmg} de daño`);
         console.log(`Estado tras el turno el luchador ${this.Fighter1.getNameCharacter()} tiene ` +
-          `${this.Fighter1.getHP()} de vida y el  ${this.Fighter2.getNameCharacter()} tiene ` +
+          `${this.Fighter1.getHP()} de vida y el luchador  ${this.Fighter2.getNameCharacter()} tiene ` +
           `${this.Fighter2.getHP() } de vida`);
       } else {
         console.log(`Turno: ${turno}`);
         console.log(`El luchador ${this.Fighter2.getNameCharacter()} realiza su ataque`);
+        console.log(`${this.Fighter2.getCatchingPhrase()}`);
         dmg = this.Fighter2.generalDamage(this.Fighter2, this.Fighter1);
         this.Fighter1.setHP(this.Fighter1.getHP() - dmg);
         console.log(`El luchador ${this.Fighter1.getNameCharacter()} recibe ${dmg} de daño`);
         console.log(`Estado tras el turno el luchador ${this.Fighter2.getNameCharacter()} tiene ` +
-          `${this.Fighter2.getHP()} de vida y el  ${this.Fighter1.getNameCharacter()} tiene ` +
+          `${this.Fighter2.getHP()} de vida y el luchador ${this.Fighter1.getNameCharacter()} tiene ` +
           `${this.Fighter1.getHP()} de vida`);
       }
       turno += 1;
